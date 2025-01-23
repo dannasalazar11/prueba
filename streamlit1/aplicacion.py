0de9875bad8b93c3dbffde370ff2c91e902dd536
dannasalazar11 <93801258+dannasalazar11@users.noreply.github.com>
2025-01-23 15:20:15 -0500

    Update boton subir archivo aplicacion.py

--- streamlit1/aplicacion.py
+++ streamlit1/aplicacion.py
@@ -4,6 +4,12 @@ def main():
   """ Función que se encargará de la ejecución principal"""
   st.title("Clasificación de Imágenes con un Modelo Preentrenado")
 
+  st.markdown("Sube una imagen para clasificar")
+
+  # widget que permite subir archivos
+  upload_file = st.fileuploader("Selecciona una imagen (PNG, JPG, JPEG):", type=["png", "jpg", "jpeg"])
+  
+
 if __name__ == "__main__":
   main()
   
