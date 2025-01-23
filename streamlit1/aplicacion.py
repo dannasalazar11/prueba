c3e4dbf975e4b98c6c6eed524fce201778456515
dannasalazar11 <93801258+dannasalazar11@users.noreply.github.com>
2025-01-23 15:44:40 -0500

    Update aplicacion.py

--- streamlit1/aplicacion.py
+++ streamlit1/aplicacion.py
@@ -38,6 +38,11 @@ def main():
 
     if st.button("Clasificar imagen"):
       st.markdown("Imagen clasificada")
+      model = load_model()
+
+      prediction = model.predict(preprocessed_image.reshape(1,-1))
+
+      st.markdown(f"La imagen fue clasificada como: {prediction}")
 
   
 if __name__ == "__main__":
