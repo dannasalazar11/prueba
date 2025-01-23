ef020df246da490055e478eebf704dc48c800777
dannasalazar11 <93801258+dannasalazar11@users.noreply.github.com>
2025-01-23 15:22:36 -0500

    Update aplicacion.py

--- streamlit1/aplicacion.py
+++ streamlit1/aplicacion.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from PIL import Image
 
 def main():
   """ Función que se encargará de la ejecución principal"""
@@ -7,8 +8,10 @@ def main():
   st.markdown("Sube una imagen para clasificar")
 
   # widget que permite subir archivos
-  upload_file = st.file_uploader("Selecciona una imagen (PNG, JPG, JPEG):", type=["png", "jpg", "jpeg"])
-  
+  uploaded_file = st.file_uploader("Selecciona una imagen (PNG, JPG, JPEG):", type=["png", "jpg", "jpeg"])
+
+  if uploaded_file is not None:
+    image = Image.open(uploaded_file)
 
 if __name__ == "__main__":
   main()
