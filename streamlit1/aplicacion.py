1e901d5bdf872b2c0d3da7ea03781e53bed068ad
dannasalazar11 <93801258+dannasalazar11@users.noreply.github.com>
2025-01-23 15:16:53 -0500

    Update aplicacion.py

--- streamlit1/aplicacion.py
+++ streamlit1/aplicacion.py
@@ -4,6 +4,6 @@ def main():
   """ Función que se encargará de la ejecución principal"""
   st.markdown("Clasificación de Imágenes con un Modelo Preentrenado")
 
-if __name__ == '__main__":
+if __name__ == "__main__":
   main()
   
