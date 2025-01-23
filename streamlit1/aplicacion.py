f3f6edaa2bff33cc253f4f846a7acddb6012faa9
dannasalazar11 <93801258+dannasalazar11@users.noreply.github.com>
2025-01-23 15:40:58 -0500

    Update aplicacion.py

--- streamlit1/aplicacion.py
+++ streamlit1/aplicacion.py
@@ -2,6 +2,14 @@ import streamlit as st
 from PIL import Image
 from tensorflow.keras.preprocessing.image import img_to_array
 import numpy as np
+import gzip
+
+def load_model():
+  filename = "'model_trained.pkl.gz"
+  with gzip.open(filename, 'rb') as f:
+        model = pickle.load(f)
+  return model
+  
 
 def preprocess_image(image):
   image = image.convert('L') # convertir a escala de grises
