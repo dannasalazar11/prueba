4a56f0885719ba2a09e57407e7210197128181a6
dannasalazar11 <93801258+dannasalazar11@users.noreply.github.com>
2025-01-23 15:15:10 -0500

    Create aplicacion.py

---
+++ streamlit1/aplicacion.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+def main():
+  """ Función que se encargará de la ejecución principal"""
+  st.markdown("Clasificación de Imágenes con un Modelo Preentrenado")
+
+if __name__ == '__main__":
+  main()
+  
